9700fdcb87c94721cf33f9b0cc78765f7e5ca406
Lucas Alves <lucascalliari172@gmail.com>
2025-04-02 18:45:16 -0300

    Criando etapa de receber arquivo CSV do usuário (falta uma manejar o erro do caso do arquivo não ser CSV)

--- analisadorDeDados/main.py
+++ analisadorDeDados/main.py
@@ -1,7 +1,19 @@
-# inicio do trabalho
+# importando bibliotecas
 import pandas as pd
-import csv
+import matplotlib as mpl
 
-minhaString = "ALOU GALERA DE COWBOY"
 
-print(minhaString)
+# Recebendo arquivo CSV com dados de alunos
+caminho_arquivo = input("Entre com o caminho do arquivo a ser manejado: ")
+try:
+  dados = pd.read_csv(caminho_arquivo)
+  print(dados)
+except FileNotFoundError:
+  print(f"Erro: arquivo {caminho_arquivo} não encontrado. Tente novamente!")
+except pd.errors.ParserError:
+  print(f"Erro: não conseguimos analisar o arquivo {caminho_arquivo}. Verifique se é um arquivo CSV válido!")
+# Limpeza de dados
+
+# Consulta aos dados
+
+# Geração dos gráficos (Matplotlib)
